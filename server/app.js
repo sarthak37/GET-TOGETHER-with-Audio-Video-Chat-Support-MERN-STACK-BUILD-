5e5b4e492ff1077416c9b5318c0315e8c969a845
sarthak37 <sarthak27998@gmail.com>
2021-08-19 21:30:06 +0530

    h

--- server/app.js
+++ server/app.js
@@ -152,6 +152,14 @@ app.use(function(err,req,res,next){
     }
 });
 
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    })
+}
+
 
 
 server.listen(PORT, () => {
